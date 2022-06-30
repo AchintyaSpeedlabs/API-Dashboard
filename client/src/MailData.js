06a0d9778d69d2624838e1864f8f319a35a5d036
Hemani Gajjar <hmngjjr17@gmail.com>
2022-06-30 23:44:27 +0530

    add table for excel data

--- client/src/MailData.js
+++ client/src/MailData.js
@@ -3,3 +3,14 @@ export const recurrenceOptions = [
   { value: "daily", label: "Daily" },
   { value: "monthly", label: "Monthly" },
 ];
+
+export const columns = [
+  { id: "firstname", label: "First Name", minWidth: 170, align: "left" },
+  { id: "lastname", label: "Last Name", minWidth: 100, align: "left" },
+  {
+    id: "email",
+    label: "Email",
+    minWidth: 170,
+    align: "left",
+  },
+];
